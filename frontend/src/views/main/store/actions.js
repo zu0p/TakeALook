28ca7c296c63dbd6f0b9b9e6c82d15419ddb761e
multicampus <a33a66a99@naver.com>
2021-07-15 13:42:29 +0900

    .

--- frontend/src/views/main/store/actions.js
+++ frontend/src/views/main/store/actions.js
@@ -16,6 +16,12 @@ export function requestSignup({state}, payload){
   return $axios.post(url, body)
 }
 
+export function requestCheckDupl({state}, payload){
+  //console.log(payload)
+  const url = `http://localhost:8080/api/v1/users/${payload.id}`
+  return $axios.get(url)
+}
+
 export function requestUserInfo({commit}, payload){
   const url = '/api/v1/users/me'
   $axios.get(url)
