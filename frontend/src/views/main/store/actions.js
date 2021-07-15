759c45cd1a52cb8dc655a97c2b1ade4d655d01e0
multicampus <a33a66a99@naver.com>
2021-07-15 16:18:24 +0900

    axios 공통으로 토큰 헤더에 담아 전송

--- frontend/src/views/main/store/actions.js
+++ frontend/src/views/main/store/actions.js
@@ -1,30 +1,39 @@
 // API
-import $axios from 'axios'
+import axios from 'axios'
+import { setInterceptors } from '../../../common/interceptors'
+
+function createInstance(){
+  const instance = axios.create()
+  return setInterceptors(instance)
+}
+const BASE_URL = 'http://localhost:8080/api/v1'
+const instanceWithAuth = createInstance()
 
 export function requestLogin ({ state, commit }, payload) {
   //console.log('requestLogin', state, payload)
-  const url = 'http://localhost:8080/api/v1/auth/login'
+  const url = BASE_URL+'/auth/login'
   let body = payload
 
-  return $axios.post(url, body)
+  //return axios.post(url, body)
+  return instanceWithAuth.post(url, body)
 }
 
 export function requestSignup({state}, payload){
-  const url = 'http://localhost:8080/api/v1/users'
+  const url = BASE_URL+'/users'
   let body = payload
 
-  return $axios.post(url, body)
+  return instanceWithAuth.post(url, body)
 }
 
 export function requestCheckDupl({state}, payload){
   //console.log(payload)
-  const url = `http://localhost:8080/api/v1/users/${payload.id}`
-  return $axios.get(url)
+  const url = BASE_URL+`/users/${payload.id}`
+  return instanceWithAuth.get(url)
 }
 
 export function requestUserInfo({commit}, payload){
-  const url = '/api/v1/users/me'
-  $axios.get(url)
+  const url = BASE_URL+'/users/me'
+  instanceWithAuth.get(url)
     .then(res=>{
       commit("SET_USER_INFO", res.data.userId)
     })
