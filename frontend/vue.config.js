6cb53e658dfc509ad1b2d6aa596e6c0222a6dfb4
Seungyeup <lsyeup1206@naver.com>
2021-07-30 09:24:59 +0900

    fix: HTTPS SSL인증

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -2,7 +2,27 @@
 // Vue3 관련 설정 파일
 module.exports = {
   devServer: {
-    https: false,
+    // local 실행 시
+    // https: false,
+    // port: 8083,
+    // open: true,
+    // proxy: {
+    //   '/api/v1': {
+    //     target: 'http://i5d101.p.ssafy.io:8080/',
+    //   }
+    // },
+    // historyApiFallback: true,
+    // hot: true,
+    // disableHostCheck: true,
+
+    // 서버 실행시
+    https: true,
+    https: {
+      key: fs.readFileSync('/etc/letsencrypt/live/i5d101.p.ssafy.io/privkey.pem'),
+      cert: fs.readFileSync('/etc/letsencrypt/live/i5d101.p.ssafy.io/cert.pem'),
+      ca: fs.readFileSync('/etc/letsencrypt/live/i5d101.p.ssafy.io/chain.pem'),
+    },
+    host: '0.0.0.0',
     port: 8083,
     open: true,
     proxy: {
@@ -13,6 +33,7 @@ module.exports = {
     historyApiFallback: true,
     hot: true,
     disableHostCheck: true,
+
   },
   css: {
     requireModuleExtension: false // import 시에 경로에 .module 포함 안해도 됨.
