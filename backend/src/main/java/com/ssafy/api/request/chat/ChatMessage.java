fc541f6f2c1f412b91627deaa57082b0166af029
김나현 <5604100@naver.com>
2021-08-10 15:28:38 +0900

    feat: 채팅 STOMP

--- backend/src/main/java/com/ssafy/api/request/chat/ChatMessage.java
+++ backend/src/main/java/com/ssafy/api/request/chat/ChatMessage.java
@@ -1,18 +1,13 @@
 package com.ssafy.api.request.chat;
 
-import io.swagger.annotations.ApiModel;
-import io.swagger.annotations.ApiModelProperty;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
-@AllArgsConstructor
 public class ChatMessage {
-    // 유저의 이름을 저장하기 위한 변수
-    private String userName;
 
-    // 메시지의 내용을 저장하기 위한 변수
-    private String content;
+    private String roomId;
+    private String writer;
+    private String message;
 }
