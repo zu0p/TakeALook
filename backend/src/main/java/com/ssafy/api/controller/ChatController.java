3119c158b104c7f32b3ae4bda0ed324d74b37e28
김나현 <5604100@naver.com>
2021-08-11 11:58:05 +0900

    fix: chat api 수정

--- backend/src/main/java/com/ssafy/api/controller/ChatController.java
+++ backend/src/main/java/com/ssafy/api/controller/ChatController.java
@@ -3,11 +3,7 @@ package com.ssafy.api.controller;
 import com.ssafy.api.request.chat.ChatMessage;
 import lombok.RequiredArgsConstructor;
 import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.handler.annotation.Payload;
-import org.springframework.messaging.simp.SimpMessageSendingOperations;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
