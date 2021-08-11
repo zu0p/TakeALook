1636e6fff2329dcc80a73039a092af8c477ba0c4
김나현 <5604100@naver.com>
2021-08-11 11:57:10 +0900

    fix:

--- backend/src/main/java/com/ssafy/api/controller/ChatRoomController.java
+++ backend/src/main/java/com/ssafy/api/controller/ChatRoomController.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
