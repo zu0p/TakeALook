1ddc94d28f0dd540507bfd29a0c6b8b719fa03f0
Seungyeup <lsyeup1206@naver.com>
2021-08-13 17:38:17 +0900

    fix: fileControll

--- backend/src/main/java/com/ssafy/api/controller/ImageController.java
+++ backend/src/main/java/com/ssafy/api/controller/ImageController.java
@@ -8,12 +8,16 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.FileCopyUtils;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import retrofit2.http.Multipart;
 
+import javax.servlet.ServletContext;
+import java.io.File;
 import java.io.IOException;
 
 @Slf4j
@@ -22,10 +26,18 @@ import java.io.IOException;
 @RequestMapping("/api/v1/image")
 public class ImageController {
 
+    @Autowired
+    ServletContext servletContext;
+
     @PostMapping(value = "/upload", consumes = {MediaType.MULTIPART_FORM_DATA_VALUE})
     @ApiOperation(value = "이미지 업로드", notes = "이미지를 받으면 서버에 저장한다.")
     public ResponseEntity<?> uploadImages(@RequestParam("imageFile") MultipartFile img) throws IOException {
         log.info("image has arrived!!!! which is {}", img.getOriginalFilename());
+        String webAppRoot = servletContext.getRealPath("/");
+        String relativeFolder = File.separator + "resources" + File.separator + "images" + File.separator;
+
+        String filename = webAppRoot + relativeFolder + img.getOriginalFilename();
+        FileCopyUtils.copy(filename.getBytes(), new File(filename));
 
         return ResponseEntity.status(200).body(BaseResponseBody.of(200,"Images arrived"));
     }
