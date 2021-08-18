f41a2cc7afe3795c9efea4a67835dd1da561dc48
ji55 <jsimbajs@gmail.com>
2021-08-18 13:36:27 +0900

    feat: 카드 이미지 url

--- frontend/src/views/home/components/conference.vue
+++ frontend/src/views/home/components/conference.vue
@@ -102,8 +102,7 @@ export default {
     const store = useStore()
     const router = useRouter()
 
-    // const imgUrl = require(`@/assets/pimages/${props.deal.productId}.jpg`)
-    const imgUrl = require(`@/assets/pimages/131.jpg`)
+    const imgUrl = require(`@/assets/pimages/${props.deal.productId}.jpg`)
     const info = reactive({
       wishCount: 0,
       isLogin: false,
