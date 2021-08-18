4adbbb63bce79d1290d4958482b1d923d56d622b
multicampus <zu0p@kakao.com>
2021-08-18 13:43:49 +0900

    chore: propmt 기본값 제거

--- frontend/src/views/mypage/my-deal.vue
+++ frontend/src/views/mypage/my-deal.vue
@@ -108,11 +108,11 @@ export default {
 
       if(param.reserveTime>now){ // 입장 가능하면
         //호가 입력받기
-        let priceGap = Number(prompt('가격 증감 단위를 입력하세요.(숫자)', '-1'))
+        let priceGap = Number(prompt('가격 증감 단위를 입력하세요.(숫자)'))
         // alert(priceGap)
         // console.log(priceGap)
-        if(priceGap == -1 || priceGap == null){
-          console.log(priceGap)
+        if(priceGap == 0 || priceGap == null){
+          //console.log(priceGap)
           return
         }
         else{
