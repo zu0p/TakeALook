ab41c80ab945763a89c3d1f6e804a197091c3212
김나현 <5604100@naver.com>
2021-08-19 14:56:02 +0900

    fix: 채팅 저장

--- backend/src/main/java/com/ssafy/api/service/chat/ChatService.java
+++ backend/src/main/java/com/ssafy/api/service/chat/ChatService.java
@@ -1,5 +1,6 @@
 package com.ssafy.api.service.chat;
 
+import com.ssafy.api.request.chat.ChatMessage;
 import com.ssafy.api.response.chat.ChatListGetRes;
 import org.json.simple.parser.ParseException;
 
@@ -7,5 +8,8 @@ import java.util.List;
 
 public interface ChatService {
     void saveChat(String chatList) throws ParseException;
+
+    void saveMessage(ChatMessage message);
+
     List<ChatListGetRes> getChatList(String roomId);
 }
